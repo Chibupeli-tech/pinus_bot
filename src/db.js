d86ea586a2e8e539eb5d5dafada5ff8360af2a64
Camisul <7963180+Camisul@users.noreply.github.com>
2021-02-02 02:18:26 +0300

    Deprecating db

--- src/db.js
+++ src/db.js
@@ -1,35 +1,4 @@
-const axios = require('axios');
-
-const config = require('../config');
 const { bannedIds } = require('./ids');
-const { reportError } = require('./error');
-const req = (q) => ({
-  jsonrpc: '2.0',
-  id: 0,
-  method: 'sql',
-  params: {
-    auth: { "user": config.DB_USER, "pass": config.DB_PASS },
-    db: config.DB_NAME,
-    query: q
-  }
-});
-
-function request(query) {
-  return axios.post(config.DB_URL, req(query))
-    .then(res => {
-      if (res.data) {
-        if ('error' in res.data) {
-          throw new Error(res.data.error.message);
-        }
-        else {
-          return res.data.result
-        }
-      }
-    })
-    .catch(error => {
-      throw error
-    });
-}
 
 function updateFromDb() {
     bannedIds.push(...['318834208741261312', '594919505956700170']);
@@ -41,4 +10,4 @@ function addToDb(id) {
 
   return;
 }
-module.exports = { request, updateFromDb, addToDb };
+module.exports = { updateFromDb, addToDb };
