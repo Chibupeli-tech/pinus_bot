2035a2ddbb7f8ac7ceae404e3114fb062823027a
Camisul <7963180+Camisul@users.noreply.github.com>
2021-04-09 06:03:46 +0300

    Move to typescript

--- src/ids.js
+++
@@ -1,6 +0,0 @@
-const bannedIds = ['318834208741261312', '594919505956700170'];
-
-const ownerId = '258680327906525184';
-const allowedIds = [ownerId, '359336905185165322'];
-
-module.exports = {bannedIds, allowedIds, ownerId};
